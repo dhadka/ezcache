24153080d4c6807c30a274b9fb1aff03fc7720b0
Dave Hadka <dhadka@github.com>
2020-10-23 21:25:01 -0500

    Add last cache

---
+++ src/handlers/other/last.ts
@@ -0,0 +1,30 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { registry, CacheHandler } from '../../registry'
+import { hashFiles, runner } from '../../expressions'
+
+class LastCache extends CacheHandler {
+    getBranchName(): string {
+        let ref = github.context.ref
+
+        if (ref.startsWith('refs/heads/')) {
+            ref = ref.substring(11)
+        }
+
+        return ref
+    }
+
+    async getPaths(): Promise<string[]> {
+        return core.getInput('path').split('\n').map(s => s.trim())
+    }
+
+    async getKey(version?: string): Promise<string> {
+        return `${runner.os}-${version}-last-${this.getBranchName()}-${await hashFiles(await this.getPaths())}`
+    }
+
+    async getRestoreKeys(version?: string): Promise<string[]> {
+        return [`${runner.os}-${version}-last-${this.getBranchName()}-`, `${runner.os}-${version}-last-master-`]
+    }
+}
+
+registry.add("last", new LastCache())
